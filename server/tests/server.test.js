9a821825348c8c4cd5040e6a6d34fc4359486a14
bvasan <bsrinivasan.pdy@gmail.com>
2018-07-17 16:18:59 -0500

    Added test cases for GET

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,17 +1,33 @@
 const expect = require('expect');
 const request = require('supertest');
+const gstore = require('gstore-node')();
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+var todos = [];
+var todo = new Todo({
+  text: 'First test todo'
+});
+todos.push(todo);
+
+var todo = new Todo({
+  text: 'Second test todo'
+});
+todos.push(todo);
+
 beforeEach((done) => {
-  Todo.deleteAll().then(() => done());
+  Todo.deleteAll().then(() => {
+    return gstore.save(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
   it('should create a new todo item', (done) => {
     var text = 'Test todo text from Mocha -updated2';
-
+    const options = {
+      filters : ['text','=',text]
+    }
     request(app)
       .post('/todos')
       .send({text})
@@ -23,7 +39,7 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         };
-        Todo.list().then((todos) => {
+        Todo.list(options).then((todos) => {
           expect(todos.entities.length).toBe(1);
           expect(todos.entities[0].text).toBe(text);
           done()
@@ -44,7 +60,7 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.list().then((todos) => {
-          expect(todos.entities.length).toBe(0);
+          expect(todos.entities.length).toBe(2);
           done();
         }).catch((e) => {
           return done(e);
@@ -53,3 +69,16 @@ describe('POST /todos', () => {
   });
 
 });
+
+describe('GET /todos ', () => {
+  it('should get all todos', (done) => {
+
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.entities.length).toBe(2);
+      })
+      .end(done);
+  });
+})
